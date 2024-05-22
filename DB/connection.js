a77c5910d07fdbd4d372b20fdfc4d4987c200213
Mohammad Zagha <tsu.test11@gmail.com>
2024-05-22 15:15:11 +0300

    Added nodemon and monogdb Con url

--- DB/connection.js
+++ DB/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async ()=>{
 
-    return await mongoose.connect("")
+    return await mongoose.connect("mongodb+srv://admin:admin@cluster0.1mpshqq.mongodb.net/")
     .then((result)=>{
 
         console.log("connect DB");
